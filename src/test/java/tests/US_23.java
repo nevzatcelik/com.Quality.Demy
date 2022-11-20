43230657baca9b3c0dec52256a4084965f205396
mtalhakurt95 <114331006+mtalhakurt95@users.noreply.github.com>
2022-11-20 17:57:10 +0100

    20.11(1) Talha

--- src/test/java/tests/US_23.java
+++ src/test/java/tests/US_23.java
@@ -1,4 +1,35 @@
 package tests;
 
+import org.testng.annotations.Test;
+import pages.QualityDemyPage_Instructor;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
 public class US_23 {
+
+    @Test
+    public void testus23() {
+        //go to qualitydemy web page
+        Driver.getDriver().get(ConfigReader.getProperty("myUrl"));
+        //click login link
+        QualityDemyPage_Instructor qdp = new QualityDemyPage_Instructor();
+        qdp.loginLink.click();
+        //Acept Cookies
+        qdp.acceptCookies.click();
+        ReusableMethods.bekle(2);
+        //fill login email adress and password
+        qdp.emailBox.sendKeys(ConfigReader.getProperty("i_Email"));
+        qdp.passwordBox.sendKeys(ConfigReader.getProperty("i_password"));
+        //click login button
+        qdp.loginButton.click();
+
+
+
+
+
+
+
+
+    }
 }
