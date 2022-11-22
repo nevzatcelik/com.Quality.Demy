de222070e5bbdc21101e9f7bc0ae3c0d8022b22e
mtalhakurt95 <114331006+mtalhakurt95@users.noreply.github.com>
2022-11-22 18:56:06 +0100

    22.11(3) Talha

--- src/test/java/tests/US_29.java
+++ src/test/java/tests/US_29.java
@@ -1,4 +1,37 @@
 package tests;
 
+import org.testng.annotations.Test;
+import pages.QualityDemyPage_Instructor;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
 public class US_29 {
+    QualityDemyPage_Instructor qdp = new QualityDemyPage_Instructor();
+
+    @Test
+    public void testus29() {
+        //go to qualitydemy web page
+        Driver.getDriver().get(ConfigReader.getProperty("myUrl"));
+        //click login link
+        qdp.loginLink.click();
+        //Acept Cookies
+        qdp.acceptCookies.click();
+        ReusableMethods.bekle(2);
+        //fill login email adress and password
+        qdp.emailBox.sendKeys(ConfigReader.getProperty("i_Email"));
+        qdp.passwordBox.sendKeys(ConfigReader.getProperty("i_password"));
+        //click login button
+        qdp.loginButton.click();
+        ReusableMethods.bekle(2);
+        //click Instructor from Homepage
+        qdp.Instructor.click();
+        ReusableMethods.bekle(2);
+        //Click Course Manager
+        qdp.c_manager.click();
+        ReusableMethods.bekle(2);
+        //Click Add new course
+        qdp.add_newcourse.click();
+        ReusableMethods.bekle(2);
+    }
 }
