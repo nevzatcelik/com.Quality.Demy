92b6976afc092180280972691c22a9188d9bd7b8
Bora-Se <114330033+Bora-Se@users.noreply.github.com>
2022-11-25 22:42:33 +0100

    25.11 (1) Bora

--- src/test/java/pages/QualityDemyPage_Instructor.java
+++ src/test/java/pages/QualityDemyPage_Instructor.java
@@ -1525,82 +1525,82 @@ public WebElement categories;
 
 //-------------------------1526==1675---------------BORA------------------------------------------------------//
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //Home Page
+    @FindBy (xpath = "//img[@src='https://qualitydemy.com/uploads/system/393645bf994114c23be28028a47a77a0.png']")
+    public WebElement homePage;
+    //Home Page > Categories > Web Design Course   //i[@class='fas fa-bars text-16px']
+    @FindBy (xpath = "(//a[@class='py-2 text-wrap d-flex'])[1]")
+    public WebElement categoriWebDesing;
+    //Home Page > Categories > Web Design Course > Web Design for Web Developers
+    @FindBy (xpath = "//*[text()='Web Design for Web Developers']")
+    public WebElement webDevelopers;
+    //Home Page > Categories > Web Design Course > Web Design for Web Developers > Wordpress
+    @FindBy(xpath = "(//div[@class='course-image'])[3]")
+    public WebElement wordpresslink;
+    //Home Page > Categories > Web Design Course > Wordpress > Buy now
+    @FindBy (xpath = "//*[text()='Buy now']")
+    public WebElement wordpressBuyNow;
+    //Home Page > Shopping
+    @FindBy(xpath = "(//a[@href='https://qualitydemy.com/home/shopping_cart'])[1]")
+    public WebElement shoppingButton;
+    //Home Page > Shopping > checkout
+    @FindBy (xpath = "//button[@class='btn red w-100 radius-10 mb-3']")
+    public WebElement checkoutButton;
+    //Home Page > Shopping >  'Wordpress' course > checkout > stripe
+    @FindBy (xpath = "//img[@class='payment-gateway-icon']")
+    public WebElement stripeButoon;
+    //Home Page > Shopping >  'Wordpress' course > checkout > stripe > payWithStripe
+    @FindBy (xpath = "//button[@class='stripe-button payment-button float-end']")
+    public WebElement payWithStripe;
+    //Home Page > Shopping >  'Wordpress' course > checkout > close button
+    @FindBy (xpath = "//i[@class='fa fa-times']")
+    public WebElement closeButton;
+    //Home Page > Shopping Card Page
+    @FindBy (xpath = "//li[@class='breadcrumb-item active text-light display-6 fw-bold']")
+    public WebElement shoppingCardPage;
+    //Home Page > Shopping Card Page > Checkout sprite Page
+    @FindBy (xpath = "(//input[@class='CheckoutInput Input Input--empty'])[1]")
+    public WebElement checkoutEmail;
+    //Home Page > Shopping Card Page > Checkout sprite Page
+    @FindBy (xpath = "//input[@autocomplete='cc-number']")
+    public WebElement cardNummer;
+    //Home Page > Shopping Card Page > Checkout sprite Page
+    @FindBy (xpath = "//input[@autocomplete='cc-exp']")
+    public WebElement expDatum;
+    //Home Page > Shopping Card Page > Checkout sprite Page
+    @FindBy(xpath = "//input[@autocomplete='cc-csc']")
+    public WebElement cardCVC;
+    //Home Page > Shopping Card Page > Checkout sprite Page
+    @FindBy (xpath = "//input[@autocomplete='ccname']")
+    public WebElement nameOnCard;
+    //Home Page > Shopping Card Page > Checkout sprite Page
+    @FindBy (xpath = "//select[@autocomplete='billing country']")
+    public WebElement countryName;
+    //Home Page > Shopping Card Page > Checkout sprite Page
+    @FindBy(xpath = "//*[text()='Sweden']")
+    public WebElement countrySweden;
+
+    // Home Page > MyCourses
+    @FindBy(xpath = "(//*[text()='My courses'])[1]")
+    public WebElement myCourses;
+    // Home Page > MyCourses > Wordpress
+    @FindBy (xpath = "//img[@src='https://qualitydemy.com/uploads/thumbnails/course_thumbnails/optimized/course_thumbnail_default_31666766582.jpg']")
+    public WebElement myCoursePageWordpres;
+    //
+    @FindBy(xpath = "//div[@class='SubmitButton-IconContainer']")
+    public WebElement payButton;
+
+    //=======================================================================
+
+    //Home Page > Categories > Web Design Course > HTML5 and CSS
+    @FindBy (xpath = "(//*[text()='HTML5 and CSS'])[1]")
+    public WebElement HTMLandCSS;
+    //Home Page > Categories > Web Design Course > Web Design for Web Developers > Build Responsive Real-World Websites with HTML and CSS
+    @FindBy(xpath = "//img[@src='https://qualitydemy.com/uploads/thumbnails/course_thumbnails/optimized/course_thumbnail_default_12.jpg']")
+    public WebElement buildResponsiRealWorldHTMLandCSSlink;
+    //Home Page > Categories > Web Design Course > Build Responsive Real-World Websites with HTML and CSS > Buy now
+    @FindBy (xpath = "//*[text()='Buy now']")
+    public WebElement courseBuyNow;
 
 
 
--- src/test/java/pages/QualityDemyPage_User.java
+++ src/test/java/pages/QualityDemyPage_User.java
@@ -1527,84 +1527,84 @@ public WebElement homePage;
 
 
 
+    //Home Page > Top Courses > Web Design for PHP
+    @FindBy(xpath = "//h5[text()='Web Design for PHP']")
+    public WebElement webDesignCourse;
 
+    //Home Page > Top Courses > Web Design for PHP > Compare this course with other
+    @FindBy(xpath = "//a[@href='https://www.qualitydemy.com/home/compare?course-1=web-design-for-php&&course-id-1=1']")
+    public WebElement compareThisButton;
 
+    //Home Page > Home > Course compare
+    @FindBy(xpath = "//li[@class='breadcrumb-item active text-light display-6 fw-bold']")
+    public WebElement courseCompare;
 
+    //Home Page > Top Courses > English Course Learn To Speak
+    @FindBy(xpath = "//h5[text()='English Course Learn To Speak']")
+    public WebElement englishCourse;
 
+    //Home ==> /home/course/english-course-learn-to-speak
+    @FindBy(xpath = "//button[text()='Buy now']")
+    public WebElement buyNowButton;
 
+    @FindBy(xpath = "//button[text()='Checkout']")
+    public WebElement checkoutButton;
 
+    //Home ==> /home/course/english-course-learn-to-speak
+    @FindBy(xpath = "//button[text()='Add to Cart']")
+    public WebElement addToCartButton;
 
+    //Home ==> /home/course/english-course-learn-to-speak
+    @FindBy(xpath = "//button[text()='Added to Cart']")
+    public WebElement addedToCartButton;
 
+    //Home ==> /home/course/Go to cart
+    @FindBy(xpath = "//div[@id='cart_items']")
+    public WebElement cartButton;
 
+    //Home / Shopping Cart
+    @FindBy(xpath = " (//div[@class='cart-course-wrapper box-shadow-5'])[2] ")
+    public WebElement shoppingCart;
 
 
+    //Home / English Course Learn To Speak--> Add/Added yakalama
+    @FindBy(xpath = "(//img[@data-bs-original-title='Admin User'])[3]")
+    public WebElement moveToElementEnglish;
 
 
+    //Home / Course / Shopping Cart /English Course Learn To Speak--> Sepete eklendi
+    @FindBy(xpath = "(//div[text()='English Course Learn To Speak'])[3]")
+    public WebElement englishCourseShoppingCart;
 
+    //Home ==> /home/course/english-course-learn-to-speak/ Add to wishlist
+    @FindBy(xpath = "//button[@onclick='handleAddToWishlist(this)']")  // (//button[@id='4'])[1]   //button[text()='Add to wishlist']
+    public WebElement addTowishlistButton;
 
+    //Home ==> /Home/course/english-course-learn-to-speak / Added to wishlist
+    @FindBy(xpath = "//button[text()='Added to wishlist']")
+    public WebElement addedTowishlistButton;
 
+    //Home ==> /home/course/english-course-learn-to-speak / My courses //div[@id='wishlist_items']
+    @FindBy(xpath = "(//a[text()='My courses'])[1]")
+    public WebElement myCoursesButton;
 
+    //Home ==> /home/my_course/ Wishlist //div[@id='wishlist_items']
+    @FindBy(xpath = "(//a[@href='https://www.qualitydemy.com/home/my_wishlist'])[3]")
+    public WebElement wishlistsButton;
 
+    //Home / Course / Shopping Cart /English Course Learn To Speak--> Sepete eklendi
+    @FindBy(xpath = "//h5[text()='English Course Learn To Speak']")
+    public WebElement englishCourseMyWishlist;  //h5[text()='Learn Python: The Complete Python Programming Course']
 
 
+    @FindBy(xpath = "//h5[text()='Learn Python: The Complete Python Programming Course']")
+    public WebElement learnPythonCourseDersi;
 
+    @FindBy(xpath = "//h5[text()='Web Design for PHP']")
+    public WebElement webDesignCourseForPHP;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    @FindBy(xpath = "//a[text()='Get Enrolled']")
+    public WebElement getEnrolledwebDesignCourseButton;
 
 
 
